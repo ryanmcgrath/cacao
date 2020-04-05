ba59b061772474a0a17d7ce3b1ad3a1e726b5f32
Ryan McGrath <ryan@rymc.io>
2020-04-04 21:12:17 -0700

    Ensure macOS still works, and make these things call super when need be

--- src/view/traits.rs
+++ src/view/traits.rs
@@ -10,16 +10,16 @@ pub trait ViewDelegate {
     fn did_load(&self, _view: View) {}
 
     /// Called when this is about to be added to the view heirarchy.
-    fn will_appear(&self, animated: bool) {}
+    fn will_appear(&self, _animated: bool) {}
 
     /// Called after this has been added to the view heirarchy.
-    fn did_appear(&self, animated: bool) {}
+    fn did_appear(&self, _animated: bool) {}
 
     /// Called when this is about to be removed from the view heirarchy.
-    fn will_disappear(&self, animated: bool) {}
+    fn will_disappear(&self, _animated: bool) {}
 
     /// Called when this has been removed from the view heirarchy.
-    fn did_disappear(&self, animated: bool) {}
+    fn did_disappear(&self, _animated: bool) {}
 
     /// Invoked when the dragged image enters destination bounds or frame; returns dragging operation to perform.
     fn dragging_entered(&self, _info: DragInfo) -> DragOperation { DragOperation::None }
