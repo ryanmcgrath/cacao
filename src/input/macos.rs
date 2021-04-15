5f2f1f21b092f4f2b63963d307119e2cc100d3ad
Ryan McGrath <ryan@rymc.io>
2021-04-14 22:47:13 -0700

    Fix NSTextField string dealloc bug

--- src/input/macos.rs
+++ src/input/macos.rs
@@ -13,7 +13,7 @@ use crate::utils::load;
 /// Called when editing this text field has ended (e.g. user pressed enter).
 extern "C" fn text_did_end_editing<T: TextFieldDelegate>(this: &mut Object, _: Sel, _info: id) {
     let view = load::<T>(this, TEXTFIELD_DELEGATE_PTR);
-    let s = NSString::from_retained(unsafe { msg_send![this, stringValue] });
+    let s = NSString::retain(unsafe { msg_send![this, stringValue] });
     view.text_did_end_editing(s.to_str());
 }
 
@@ -49,7 +49,7 @@ extern "C" fn text_should_end_editing<T: TextFieldDelegate>(
     _info: id,
 ) -> BOOL {
     let view = load::<T>(this, TEXTFIELD_DELEGATE_PTR);
-    let s = NSString::from_retained(unsafe { msg_send![this, stringValue] });
+    let s = NSString::retain(unsafe { msg_send![this, stringValue] });
     match view.text_should_end_editing(s.to_str()) {
         true => YES,
         false => NO
