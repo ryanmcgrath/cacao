59d74a561540bf62c57fb92b7298f67de5a14fc6
Sebastian Imlay <sebastian.imlay@gmail.com>
2023-06-08 22:24:27 -0400

    cargo fmt

--- src/uikit/scene/config.rs
+++ src/uikit/scene/config.rs
@@ -2,7 +2,9 @@ use objc::runtime::Object;
 use objc::{class, msg_send, sel, sel_impl};
 use objc_id::Id;
 
-use crate::foundation::{id, ClassMap, load_or_register_class, NSString};
++use crate::foundation::{id, load_or_register_class, ClassMap, NSString};
+
+
 use crate::uikit::scene::SessionRole;
 
 /// A wrapper for UISceneConfiguration.
