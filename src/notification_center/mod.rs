f6b5d7211463b2be26e251e0dcd4e2a0ded470d7
Adam Gastineau <adam@agg.im>
2023-07-21 11:20:13 -0700

    NSNotification and basic NSNotificationCenter

--- src/notification_center/mod.rs
+++ src/notification_center/mod.rs
@@ -15,16 +15,23 @@
 //use std::sync::Mutex;
 //use std::collections::HashMap;
 
+use block::ConcreteBlock;
 //use lazy_static::lazy_static;
-//use objc::{class, msg_send, sel, sel_impl};
-//use objc::runtime::Object;
-//use objc_id::ShareId;
+use objc::runtime::Object;
+use objc::{class, msg_send, sel, sel_impl};
+use objc_id::{Id, ShareId};
 
 mod name;
 pub use name::NotificationName;
 
+mod notification;
+
 mod traits;
 pub use traits::Dispatcher;
+
+use crate::foundation::{id, nil, NSString, Retainable};
+
+use self::notification::Notification;
 
 /*lazy_static! {
     pub static ref DefaultNotificationCenter: NotificationCenter = {
@@ -40,23 +47,62 @@ pub use traits::Dispatcher;
 
 // Wraps a reference to an `NSNotificationCenter` instance. Currently this only supports the
 // default center; in the future it should aim to support custom variants.
-//#[derive(Debug)]
-//pub struct NotificationCenter {
-//    pub objc: ShareId<Object>,
-//pub subscribers: Mutex<HashMap<String, Vec<Dispatcher>>>
-//}
+#[derive(Debug)]
+pub struct NotificationCenter {
+    pub objc: ShareId<Object>,
+    // pub subscribers: Mutex<HashMap<String, Vec<Dispatcher>>>
+}
 
-/*impl Default for NotificationCenter {
+impl Default for NotificationCenter {
     /// Returns a wrapper over `[NSNotificationCenter defaultCenter]`. From here you can handle
     /// observing, removing, and posting notifications.
     fn default() -> Self {
         NotificationCenter {
-            objc: unsafe {
-                ShareId::from_ptr(msg_send![class!(NSNotificationCenter), defaultCenter])
-            }
+            objc: unsafe { ShareId::from_ptr(msg_send![class!(NSNotificationCenter), defaultCenter]) },
         }
     }
-}*/
+}
+
+impl NotificationCenter {
+    /// Adds an entry to the notification center to receive notifications that passed to the provided block.
+    /// Corresponds to `addObserverForName:object:queue:usingBlock:`
+    ///
+    /// TODO: Missing `object` and `queue` properties, so this receives all notifications matching the name, and
+    /// on the same thread as the notification was posted
+    pub fn observe<F: Fn(Notification) -> () + Send + Sync + 'static>(&self, name: Option<NotificationName>, block: F) -> id {
+        let block = ConcreteBlock::new(move |ctx| {
+            let notification = Notification::retain(ctx);
+            block(notification);
+        });
+        let block = block.copy();
+
+        if let Some(name) = name {
+            let name: NSString = name.into();
+
+            let id: id = unsafe { msg_send![self.objc, addObserverForName: name object: nil queue: nil usingBlock: block] };
+
+            id
+        } else {
+            let id: id = unsafe { msg_send![self.objc, addObserverForName: nil object: nil queue: nil usingBlock: block] };
+
+            id
+        }
+    }
+}
+
+impl Retainable for NotificationCenter {
+    fn retain(handle: id) -> Self {
+        NotificationCenter {
+            objc: unsafe { Id::from_ptr(handle) },
+        }
+    }
+
+    fn from_retained(handle: id) -> Self {
+        NotificationCenter {
+            objc: unsafe { Id::from_retained_ptr(handle) },
+        }
+    }
+}
 
 /*impl NotificationCenter {
     pub fn observe<T: Dispatcher>(&self, name: &str, handler: &T) {
